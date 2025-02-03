2df35ceaf2e3d3fe00904f5b07dc277295fd90b4
phnxtrill <quentin_retory@hotmail.fr>
2025-02-03 10:59:18 -0400

    Création de la page ressources, liaison à la base de données

--- src/views/Resources.vue
+++ src/views/Resources.vue
@@ -1,13 +1,13 @@
 <template>
   <section class="bg-white min-h-screen">
     <div class="container px-6 py-10 mx-auto">
-      <h1 class="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl dark:text-white">Ressources</h1>
+      <h1 class="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl">Ressources</h1>
 
       <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 lg:grid-cols-2">
         <div v-for="resource in resources" :key="resource.id" @click="openModal(resource.titre, resource.description, resource.url)" class="flex items-end overflow-hidden bg-cover rounded-lg h-96 cursor-pointer" :style="{ backgroundImage: `url(${resource.image_url})` }">
-          <div class="w-full px-8 py-4 overflow-hidden rounded-b-lg backdrop-blur-sm bg-white/60 dark:bg-gray-800/60">
-            <h2 class="mt-4 text-xl font-semibold text-gray-800 capitalize dark:text-white">{{ resource.titre }}</h2>
-            <p class="mt-2 text-lg tracking-wider text-blue-500 uppercase dark:text-blue-400">{{ resource.type }}</p>
+          <div class="w-full px-8 py-4 overflow-hidden rounded-b-lg backdrop-blur-sm bg-white/60 ">
+            <h2 class="mt-4 text-xl font-semibold text-gray-800 capitalize ">{{ resource.titre }}</h2>
+            <p class="mt-2 text-lg tracking-wider text-blue-500 uppercase">{{ resource.type }}</p>
           </div>
         </div>
       </div>
